1472291e0a158326dffa8eb70a2fe6f87425983f
Lester <ldiaz@uci.cu>
2025-03-06 21:33:39 -0500

    screens creadas y barril actualizado

---
+++ lib/presentations/screens/infinit_scroll/infinit_scroll_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InfinitScrollScreen extends StatelessWidget {
+  const InfinitScrollScreen({super.key});
+  static const name = 'infinit_screen';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
